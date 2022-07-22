e7e00c54ee9c5023ab6d9d40d5ba05eee2c8af6a
Abadur Rahman Imtiaz <arimtiaz@Abadurs-MacBook-Pro.local>
2022-07-22 11:23:46 +0600

    Almost Done

--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
-
-const searchPhone = () =>{
-    const searchField = document.getElementById('search-field');
+const searchPhone = () => {
+    const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
-    console.log(searchText);    
-
-    searchField.value = '';
-
+    console.log(searchText);
+  
+    searchField.value = "";
+  
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
-    `;
-
-    fetch(url)  
-    .then(res => res.json())
-    .then(data => console.log(data.data))
-}
-
-const displaySearchResult = phone => {
-    const searchResult = document.getElementById('search-result');
+      `;
+  
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displaySearchResult(data.data));
+  };
+  
+  const displaySearchResult = (phone) => {
+    const searchResult = document.getElementById("search-result");
     console.log(searchResult);
-    phone.forEach(phone => {
-        console.log(phone);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">""</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    phone.forEach((phone) => {
+      console.log(phone);
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+          <div class="card">
+          <img src=${phone.image} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${phone.brand}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button>Details</button>
+          </div>
         </div>
       </div>
-    </div>
-        `
-    searchResult.appendChild(div);
-    })
-};
-
-
-
+          `;
+      searchResult.appendChild(div);
+    });
+  };
