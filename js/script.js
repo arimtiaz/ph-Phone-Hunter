0fc33d7fb69ce2667b4a49a0503fef1ba918dfa4
Abadur Rahman Imtiaz <arimtiaz@Abadurs-MacBook-Pro.local>
2022-07-22 12:21:54 +0600

    Scripting

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ const searchPhone = () => {
           <div class="card p-3 rounded">
           <img src=${phone.image} class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">${phone.brand}</h5>
+            <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <button id="details-button" onclick="showDetails()" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Details
